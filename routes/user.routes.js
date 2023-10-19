35caef9a3562d9b4c53e157ec0b61848e1fd3443
raditya <raditya.hanarta@gmail.com>
2023-10-19 15:01:52 +0700

    seg

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,19 @@
 const express = require('express')
+var body = require("body-parser");
+
 const app = express()
+
 app.use(express.json())
+
 const userController = require('../controllers/user.controller')
+const auth = require("../auth/auth");
+const { checkRole } = require("../middleware/checkRole");
+
 app.post("/login", userController.login)
-app.get("/getAll", userController.getAlluser)
-app.get("/findOne/")
+app.get("/getAll", auth.authVerify, checkRole(["admin","resepsionis"]), userController.getAlluser);
+app.get("/findOne/:id", auth.authVerify, checkRole(["admin","resepsionis"]), userController.finduser)
 app.post("/", userController.adduser)
 app.post("/find", userController.finduser)
 app.put("/:id", userController.updateuser)
-app.delete("/:id", userController.deleteuser)
+app.delete("/:id", auth.authVerify, checkRole(["admin"]), userController.deleteuser)
 module.exports = app
